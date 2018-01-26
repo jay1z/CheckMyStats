2a107ccf3ac3a2b972842f7df0b05ab65e0bc999
Jason A. Zurowski <jay1_z@yahoo.com>
2018-01-26 17:26:12 -0500

    Test change for auto upload after commit

--- _application/config.php
+++ _application/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://www.checkmystats.net/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
